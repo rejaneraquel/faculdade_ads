fc68f7ef45d1b12610a9b09f9ccdbb397009836f
Rejane Lima <rejane.lima87@gmail.com>
2025-09-12 15:04:14 -0300

    Exercicios de printf e scanf

--- logicaProgramacao/execercicioVariaveisScanf.c
+++ logicaProgramacao/execercicioVariaveisScanf.c
@@ -3,9 +3,25 @@
 
 int main()
 {
+  //Declarando as variáveis
   int idade;
+  char nome[20];
+  char opcao;
+  
+  // idade
   printf("Digite sua idade: \n");
   scanf("%d", &idade);
   printf("Sua idade é: %d\n", idade);
+  
+  //nome
+  printf("\nDigite seu nome: \n");
+  scanf("%s", nome);
+  printf("O nome é: %s\n", nome);
+  
+  // 1 caracter
+  printf("\nDigite a opção: \n");
+  scanf(" %c", &opcao);
+  printf("A opção é: %c\n", opcao);
+  
   return 0;
 }

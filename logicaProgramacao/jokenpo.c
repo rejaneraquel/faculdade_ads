74082c6617210ea06b505f834dfdfcc0e58b503c
Rejane Lima <rejane.lima87@gmail.com>
2025-09-22 11:38:43 -0300

    Adiciona escolha computador

--- logicaProgramacao/jokenpo.c
+++ logicaProgramacao/jokenpo.c
@@ -6,18 +6,24 @@ int main() {
   // Criando variáveis
   int escolhaJogador, escolhaComputador;
 
-  //Incializando o número aleatório
+  // Inicializando o número aleatório
   srand(time(0));
 
-
+  // Introdução
   printf("Bem Vindo ao Jogo Jokenpô!!\n");
   printf("Escolha uma opção:\n");
   printf("1. Pedra\n");
   printf("2. Papel\n");
   printf("3. Tesoura\n");
+  
+  // Usuário
   printf("Digite o número escolhido\n");
   scanf("%d", &escolhaJogador);
-  printf("Testando: %d\n", escolhaJogador);
+  printf("Escolha Usuário: %d\n", escolhaJogador);
+
+  // Computador
+  escolhaComputador = rand() % 3 + 1;
+  printf("Escolha Computador: %d\n", escolhaComputador);
 
   return 0;
 }

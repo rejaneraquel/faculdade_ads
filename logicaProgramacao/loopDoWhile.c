ac87dd753c528998bfc51619f9366da6eb565945
Rejane Lima <rejane.lima87@gmail.com>
2025-09-26 13:29:11 -0300

    do while

---
+++ logicaProgramacao/loopDoWhile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+  // Ao digitar um número impar o programa sai.
+  int i;
+  do
+  {
+    printf("Digite um número:\n");
+    printf("->Ímpar para continuar jogando!\n");
+    printf("->Par para sair do jogo.\n");
+
+    scanf("%d", &i);
+    if (i % 2 == 0)
+    {
+      printf("%d é par!!\n", i);
+    } else {
+      printf("%d é ímpar\n", i);
+    }
+  } while (i % 2 != 0);
+
+  printf("Jogo acabou!\n");
+
+  return 0;
+}

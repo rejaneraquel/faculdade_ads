e7e6a868315bbd4cc828380a4658510ee51d0c1a
Rejane Lima <rejane.lima87@gmail.com>
2025-09-27 21:55:32 -0300

    codigoTesteExercicio

---
+++ logicaProgramacao/teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int num, ind, soma = 0;
+  for (ind = 1; ind <= 5; ind ++)
+    {
+      scanf("%d", &num);
+      if (num % 2 == 0)
+        soma = soma + num;
+    };
+    printf("%d", soma);
+}

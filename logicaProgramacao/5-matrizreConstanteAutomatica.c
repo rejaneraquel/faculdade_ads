b9e5a0a3bede988eb3b2e11dede3c8953cfd7212
Rejane Lima <rejane.lima87@gmail.com>
2025-09-30 08:20:18 -0300

    Matriz linha e colunas com constante

---
+++ logicaProgramacao/5-matrizreConstanteAutomatica.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Definindo tamanho da Matriz automaticamente
+#define LINHAS 5
+#define COLUNAS 5
+
+int main(){
+
+  int matriz[LINHAS][COLUNAS];
+
+  int soma = 0;
+
+  for (int i = 0; i < LINHAS; i++) {
+    for (int j = 0; j < COLUNAS; j++) {
+      soma++;
+
+      // Atribuindo o valor do index
+      matriz[i][j] = soma;
+      printf("%d", matriz[i][j]);
+
+    }
+    printf("\n");
+  }
+
+    return 0;
+}
